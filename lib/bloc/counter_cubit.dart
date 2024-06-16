ad40bb7331d7ba0eb53c9d8d84f9606211b8342d
Astrum93 <jungmin2670@gmail.com>
2024-06-17 00:42:55 +0900

    240617 multifly 함수 추가

---
+++ lib/bloc/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<dynamic> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  void decrement() => emit(state - 1);
+
+  void multiply() => emit(state * 2);
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bloc_practice/bloc/counter_cubit.dart';
+import 'package:bloc_practice/screen/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const BlocPractice());
+}
+
+class BlocPractice extends StatelessWidget {
+  const BlocPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: const CounterScreen(),
+      ),
+    );
+  }
+}
