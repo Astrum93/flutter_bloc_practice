1474f1c55004d479dae644a1bc489e9ba6211db6
Astrum93 <jungmin2670@gmail.com>
2024-06-19 22:28:32 +0900

    240619 counter_screen에 BlocBuilder를 통해 CounterCubit의 메서드들을 사용할 수 있게 수정

--- lib/screen/counter/counter_screen.dart
+++ lib/screen/counter/counter_screen.dart
@@ -1,10 +1,46 @@
+import 'package:bloc_practice/bloc/cubit/counter_cubit.dart';
+import 'package:bloc_practice/common/widget/width_height_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterScreen extends StatelessWidget {
   const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Page'),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<CounterCubit, dynamic>(
+        builder: (context, count) => Center(
+          child: Text(
+            '$count',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          height10,
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+          height10,
+          FloatingActionButton(
+            child: const Icon(Icons.close),
+            onPressed: () => context.read<CounterCubit>().multiply(),
+          ),
+        ],
+      ),
+    );
   }
 }
