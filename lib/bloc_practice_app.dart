53a7fb4fbaa4300ebd02b567940bd71b2ccd1eae
Astrum93 <jungmin2670@gmail.com>
2024-06-19 22:03:24 +0900

    240619 GoRouter 정의 bloc_practice_app으로 이동

--- lib/bloc_practice_app.dart
+++ lib/bloc_practice_app.dart
@@ -1,13 +1,35 @@
 import 'package:bloc_practice/screen/bloc_practice_screen.dart';
+import 'package:bloc_practice/screen/counter/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BlocPracticeApp extends StatelessWidget {
   const BlocPracticeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: BlocPracticeScreen(),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    /// Home 화면
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const BlocPracticeScreen();
+      },
+      routes: [
+        GoRoute(
+          path: 'counter',
+          builder: (context, state) {
+            return const CounterScreen();
+          },
+        )
+      ],
+    )
+  ],
+);
