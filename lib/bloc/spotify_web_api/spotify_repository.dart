9d6628502c202c3b8dc6ab4afeed745a6631616a
Astrum93 <jungmin2670@gmail.com>
2024-06-26 23:16:00 +0900

    240624 spotify_repository 생성 및 음악 검색 요청 메서드 추가

--- lib/bloc/spotify_web_api/spotify_repository.dart
+++ lib/bloc/spotify_web_api/spotify_repository.dart
@@ -3,10 +3,11 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-abstract mixin class SpotifyWebApiService {
+class SpotifyRepository {
   final String clientId = dotenv.env['CLIENT_ID'] ?? '';
   final String clientSecret = dotenv.env['CLIENT_SECRET'] ?? '';
 
+  /// AccessToken 생성
   Future<String> getAccessToken() async {
     final response = await http.post(
       Uri.parse('https://accounts.spotify.com/api/token'),
@@ -26,7 +27,8 @@ abstract mixin class SpotifyWebApiService {
     }
   }
 
-  Future searchMusic(String query) async {
+  /// 음악 검색 요청
+  Future<List<dynamic>> searchMusic(String query) async {
     final token = await getAccessToken();
     final response = await http.get(
       Uri.parse('https://api.spotify.com/v1/search?q=$query&type=track'),
@@ -44,7 +46,7 @@ abstract mixin class SpotifyWebApiService {
         final String trackName = item['name'];
         final String artistsName = item['artists'][0]['name'];
 
-        Map<dynamic, dynamic> trackInfo = {
+        Map<String, dynamic> trackInfo = {
           'trackImage': trackImage,
           'trackName': trackName,
           'artistsName': artistsName,
