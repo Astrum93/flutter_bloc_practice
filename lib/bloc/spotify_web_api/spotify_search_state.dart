64bcf2645a371ae3c23ea0fbe7f610d8139c1cdc
Astrum93 <jungmin2670@gmail.com>
2024-06-26 23:26:07 +0900

    240626 spotify_search_state에 Empty, Loading, Error, Loaded 상태 추가

--- lib/bloc/spotify_web_api/spotify_search_state.dart
+++ lib/bloc/spotify_web_api/spotify_search_state.dart
@@ -1,1 +1,38 @@
+import 'package:bloc_practice/spotify/spotify_track.dart';
+import 'package:equatable/equatable.dart';
 
+/// Bloc의 상태
+
+abstract class SpotifySearchState extends Equatable {}
+
+class Empty extends SpotifySearchState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class Loading extends SpotifySearchState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class Error extends SpotifySearchState {
+  final String message;
+
+  Error({required this.message});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [message];
+}
+
+class Loaded extends SpotifySearchState {
+  final List<SpotifyTrack> spotifyTracks;
+
+  Loaded({required this.spotifyTracks});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [spotifyTracks];
+}
