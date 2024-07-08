342359cc1ff478a16362596a58a2e0ef0831870a
Astrum93 <jungmin2670@gmail.com>
2024-07-08 22:22:48 +0900

    240708 dotenv load문 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:bloc_practice/bloc_practice_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "assets/config/.env");
   runApp(
     const BlocPracticeApp(),
   );
