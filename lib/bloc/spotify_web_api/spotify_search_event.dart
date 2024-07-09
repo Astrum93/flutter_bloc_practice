d21f9f5aade1b7c1b8d7bb9864ab582d429a9a6e
Astrum93 <jungmin2670@gmail.com>
2024-07-09 23:01:16 +0900

    240709 spotify_search_event의 SearchMusicEvent 수정

--- lib/bloc/spotify_web_api/spotify_search_event.dart
+++ lib/bloc/spotify_web_api/spotify_search_event.dart
@@ -3,7 +3,11 @@ import 'package:equatable/equatable.dart';
 abstract class SpotifySearchEvent extends Equatable {}
 
 class SearchMusicEvent extends SpotifySearchEvent {
+  final String query;
+
+  SearchMusicEvent({required this.query});
+
   @override
   // TODO: implement props
-  List<Object?> get props => [];
+  List<Object?> get props => [query];
 }
